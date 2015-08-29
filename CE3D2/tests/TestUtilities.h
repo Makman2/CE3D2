38f54cee365d61b92f3642f29e69d1f623ea3667
Makman2 <makman@alice.de>
2015-08-29 03:30:51 +0200

    TestUtilities: Add easy vector creation function
    CE3D2_CREATE_VECTOR() easily creates a vector for you. You don't need
    to instantiate a vector by size and fill in the values for each entry
    manually any more.

--- CE3D2/tests/TestUtilities.h
+++ CE3D2/tests/TestUtilities.h
@@ -3,6 +3,8 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "CE3D2/Vector.h"
+
 
 /// Warns for vector equality.
 ///
@@ -49,5 +51,40 @@
                 << a[i] << " != " << b[i] << ").");   \
     }                                                 \
 }
+
+
+/// Creates a vector from a list.
+///
+/// @param numbers The list of numbers to fill the vector with.
+/// @returns       The vector filled with numbers.
+CE3D2::Vector
+_ce3d2_create_vector_from_list(
+    std::vector<CE3D2::PrecisionType> const& numbers)
+{
+    CE3D2::Vector vector = CE3D2::Vector(numbers.size());
+    for (CE3D2::Vector::size_type i = 0; i < vector.size(); i++)
+    {
+        vector[i] = numbers[i];
+    }
+
+    return vector;
+}
+
+
+/// Creates a CE3D2::Vector.
+///
+/// Creates a vector easily by filling the given parameter numbers into the
+/// vector. The size of the vector is the same as the length of the parameter
+/// numbers given.
+///
+/// For example:
+/// CE3D2_CREATE_VECTOR(1.0f, 2.0f, 3.0f, 4.0f)
+/// This creates a 4-dimensional vector with the numbers 1, 2, 3 and 4.
+///
+/// @param ...      The parameter numbers to fill the vector with.
+/// @returns        The newly created vector.
+#define CE3D2_CREATE_VECTOR(...)    \
+    _ce3d2_create_vector_from_list( \
+        std::vector<CE3D2::PrecisionType>({__VA_ARGS__}))
 
 #endif
