e02bed03c7183756ad6ec7afed726a7a340cfb64
Makman2 <makman@alice.de>
2015-09-04 16:47:48 +0200

    Model: Add test case for transform()

--- CE3D2/tests/models/ModelTest.cpp
+++ CE3D2/tests/models/ModelTest.cpp
@@ -4,6 +4,7 @@
 
 #include "CE3D2/models/Model.h"
 #include "CE3D2/tests/TestUtilities.h"
+#include "CE3D2/transformation/Scale.h"
 
 
 BOOST_AUTO_TEST_SUITE(ModelTestSuite)
@@ -51,6 +52,25 @@ BOOST_AUTO_TEST_CASE(test_vectors)
     BOOST_CHECK_VECTORS_EQUAL(uut.get_vectors()[1], CE3D2::UnitVector(3, 2));
     vecs.clear();
     BOOST_CHECK_EQUAL(uut.get_vectors().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_ITransformable)
+{
+    CE3D2::Models::Model uut;
+    auto& vecs = uut.get_vectors();
+
+    vecs.push_back(CE3D2_CREATE_VECTOR(1.0f, 2.0f, 3.0f));
+    vecs.push_back(CE3D2_CREATE_VECTOR(-2.0f, 3.3f, 22.0f));
+    vecs.push_back(CE3D2_CREATE_VECTOR(4.0f, 2.5f, 0.0f));
+
+    CE3D2::Transformation::Scale scale_transform(
+        CE3D2_CREATE_VECTOR(2.0f, 2.0f, 2.0f));
+
+    uut.transform(scale_transform);
+
+    BOOST_CHECK_VECTORS_EQUAL(vecs[0], CE3D2_CREATE_VECTOR(2.0f, 4.0f, 6.0f));
+    BOOST_CHECK_VECTORS_EQUAL(vecs[1], CE3D2_CREATE_VECTOR(-4.0f, 6.6f, 44.0f));
+    BOOST_CHECK_VECTORS_EQUAL(vecs[2], CE3D2_CREATE_VECTOR(8.0f, 5.0f, 0.0f));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
