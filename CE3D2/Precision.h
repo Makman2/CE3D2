5f027fdc2f0cac899797bc89c64641f9e945b198
Makman2 <mak95@arcor.de>
2016-01-01 23:41:14 +0100

    Precision: Add Pi
    Define a `pi` that matches the given precision inside `PrecisionType`
    so no one needs to bother about pi and the needed precision inside
    CE3D2.

--- CE3D2/Precision.h
+++ CE3D2/Precision.h
@@ -10,6 +10,9 @@ namespace CE3D2
     /// Change this if you want to increase precision of mathematical
     /// calculations from types using this.
     using PrecisionType = float;
+
+    /// The constant `pi` matching the precision given in PrecisionType.
+    PrecisionType constexpr PI = 3.1415926535897932384626433832795028841971693f;
 }
 
 #endif
