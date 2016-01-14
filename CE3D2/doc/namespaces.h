c54578f072ed832ef6746dc547c2b9af59423137
Makman2 <makman@alice.de>
2016-01-14 13:48:43 +0100

    documentation: Include namespaces
    Namespace description will be put inside *CE3D2/doc/namespaces.h*, since
    in code the same namespace appear often due to the namespace-organized
    folder structure.

---
+++ CE3D2/doc/namespaces.h
@@ -0,0 +1,23 @@
+/// Contains the CE3D2 framework.
+namespace CE3D2
+{
+    /// Contains mathematical helper functions.
+    namespace Math
+    {
+        /// Contains mathematical helper functions for matrices.
+        namespace Matrices
+        {}
+    }
+
+    /// Contains classes for grouping vectors together into meshes.
+    namespace Models
+    {}
+
+    /// Contains facilities for rendering vectors.
+    namespace Render
+    {}
+
+    /// Contains the transformation system and common transformations.
+    namespace Transformation
+    {}
+}
