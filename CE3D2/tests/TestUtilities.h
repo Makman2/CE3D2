5ec900c62b518762e6e233164b6267cebb01bd0f
Makman2 <mak95@arcor.de>
2016-01-25 21:02:42 +0100

    TestUtilities: Remove CE3D2_CREATE_VECTOR
    `CE3D2::create_vector()` from `CE3D2/Vector.h` shall be used instead!

--- CE3D2/tests/TestUtilities.h
+++ CE3D2/tests/TestUtilities.h
@@ -241,39 +241,4 @@ _create_textsurface(std::vector<std::string> const& data)
 #define CE3D2_CREATE_TEXTSURFACE(...) \
     _create_textsurface(std::vector<std::string>({__VA_ARGS__}))
 
-
-/// Creates a vector from a list.
-///
-/// @param numbers The list of numbers to fill the vector with.
-/// @returns       The vector filled with numbers.
-CE3D2::Vector
-_ce3d2_create_vector_from_list(
-    std::vector<CE3D2::PrecisionType> const& numbers)
-{
-    CE3D2::Vector vector = CE3D2::Vector(numbers.size());
-    for (CE3D2::Vector::size_type i = 0; i < vector.size(); i++)
-    {
-        vector[i] = numbers[i];
-    }
-
-    return vector;
-}
-
-
-/// Creates a CE3D2::Vector.
-///
-/// Creates a vector easily by filling the given parameter numbers into the
-/// vector. The size of the vector is the same as the length of the parameter
-/// numbers given.
-///
-/// For example:
-/// CE3D2_CREATE_VECTOR(1.0f, 2.0f, 3.0f, 4.0f)
-/// This creates a 4-dimensional vector with the numbers 1, 2, 3 and 4.
-///
-/// @param ...      The parameter numbers to fill the vector with.
-/// @returns        The newly created vector.
-#define CE3D2_CREATE_VECTOR(...)    \
-    _ce3d2_create_vector_from_list( \
-        std::vector<CE3D2::PrecisionType>({__VA_ARGS__}))
-
 #endif
